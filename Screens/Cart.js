3a001b77ed787280c041989d982dad368bb789b3
Darshan-Nagesh <darshannagesh2002@gmail.com>
2023-06-10 16:12:46 +0530

    Cart page is done

--- Screens/Cart.js
+++ Screens/Cart.js
@@ -1,33 +1,169 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useState, useLayoutEffect } from 'react';
+import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Cart = () => {
-    const navigation=useNavigation();
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        headerShown: true,
-        title: "Cart",
-        headerTitleStyle: {
-          fontSize: 18,
-          fontWeight: "bold",
-          color: "white",
-        },
-        headerStyle: {
-          backgroundColor: "#312B66",
-          height: 80,
-          borderBottomColor: "transparent",
-          shadowColor: "transparent",
-        },
-      });
-    },[]);
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "Cart",
+      headerTitleStyle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
+      },
+      headerStyle: {
+        backgroundColor: "#312B66",
+        height: 80,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
+      },
+    });
+  }, []);
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (item) => {
+    const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+    setCartItems(updatedCart);
+  };
+
+  const calculateTotal = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    return total;
+  };
+
   return (
-    <View>
-      <Text>Cart</Text>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        {cartItems.length === 0 ? (
+          <Text style={styles.emptyCartText}>Your cart is empty</Text>
+        ) : (
+          cartItems.map((item) => (
+            <View key={item.id} style={styles.cartItem}>
+              <View style={styles.itemDetails}>
+                <Text style={styles.itemName}>{item.name}</Text>
+                <Text style={styles.itemPrice}>₹ {item.price}</Text>
+              </View>
+              <View style={styles.itemActions}>
+                <Button
+                  title="-"
+                  onPress={() => {
+                    if (item.quantity > 1) {
+                      const updatedCart = cartItems.map((cartItem) => {
+                        if (cartItem.id === item.id) {
+                          return {
+                            ...cartItem,
+                            quantity: cartItem.quantity - 1,
+                          };
+                        }
+                        return cartItem;
+                      });
+                      setCartItems(updatedCart);
+                    }
+                  }}
+                />
+                <Text style={styles.itemQuantity}>{item.quantity}</Text>
+                <Button
+                  title="+"
+                  onPress={() => {
+                    const updatedCart = cartItems.map((cartItem) => {
+                      if (cartItem.id === item.id) {
+                        return {
+                          ...cartItem,
+                          quantity: cartItem.quantity + 1,
+                        };
+                      }
+                      return cartItem;
+                    });
+                    setCartItems(updatedCart);
+                  }}
+                />
+                <Button
+                  title="Remove"
+                  onPress={() => removeFromCart(item)}
+                  color="#ff0000"
+                />
+              </View>
+            </View>
+          ))
+        )}
+      </ScrollView>
+      {cartItems.length > 0 && (
+        <View style={styles.totalContainer}>
+          <Text style={styles.totalText}>Total: ₹ {calculateTotal()}</Text>
+          <Button title="Place Order" onPress={() => {}} />
+        </View>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Cart
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 20,
+  },
+  emptyCartText: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  cartItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  itemDetails: {
+    flex: 1,
+    marginRight: 10,
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  itemPrice: {
+    fontSize: 14,
+    color: '#888',
+  },
+  itemActions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  itemQuantity: {
+    fontSize: 16,
+    marginHorizontal: 10,
+  },
+  totalContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+  },
+  totalText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Cart;
