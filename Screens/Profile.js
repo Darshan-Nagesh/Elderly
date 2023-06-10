a309e4aa86e3ea699a3d7ff9758ef8126def1074
Darshan-Nagesh <darshannagesh2002@gmail.com>
2023-06-10 16:14:02 +0530

    Basic profile page is done

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,33 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useState, useLayoutEffect } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Profile = () => {
-    const navigation=useNavigation();
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        headerShown: true,
-        title: "My account",
-        headerTitleStyle: {
-          fontSize: 18,
-          fontWeight: "bold",
-          color: "white",
-        },
-        headerStyle: {
-          backgroundColor: "#312B66",
-          height: 80,
-          borderBottomColor: "transparent",
-          shadowColor: "transparent",
-        },
-      });
-    },[]);
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "My Account",
+      headerTitleStyle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
+      },
+      headerStyle: {
+        backgroundColor: "#312B66",
+        height: 80,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
+      },
+    });
+  }, []);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      {isLoggedIn ? (
+        <View>
+          <Text style={styles.loggedInText}>Welcome to Elderly!</Text>
+          <Button title="Logout" onPress={handleLogout} />
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.loggedOutText}>Please login to access your account</Text>
+          <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+  },
+  loggedInText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  loggedOutText: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  loginButton: {
+    backgroundColor: '#312B66',
+    padding: 10,
+    borderRadius: 5,
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Profile;
